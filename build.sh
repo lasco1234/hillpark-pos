efb4fcc37f3657e34388c8cae2cd4754314ba98f
lasco1234 <lascontenga32@gmail.com>
2026-08-29 18:15:03 +0300

    Update project

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ python manage.py shell << EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('lasco, 'lascontenga32@gmail.com', '90515272Lasco.')
+    User.objects.create_superuser('lasco', 'lascontenga32@gmail.com', '90515272Lasco.')
     print("Superuser created successfully")
 else:
     print("Superuser already exists")
